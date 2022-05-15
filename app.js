45a839c54bc8fca4204f617d06fb715b3abe85d6
acheng-01 <91927604+acheng-01@users.noreply.github.com>
2022-05-14 22:27:38 -0700

    Delete pseudocode

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 'use strict';
-/**
- * Example store structure
- */
+
 const store = {
   // 5 or more questions are required
   questions: [
@@ -110,21 +108,6 @@ const store = {
   questionNumber: 0,
   score: 0
 };
-
-/**
- * 
- * Technical requirements:
- * 
- * Your app should include a render() function, that regenerates the view each time the store is updated. 
- * See your course material and access support for more details.
- *
- * NO additional HTML elements should be added to the index.html file.
- *
- * You may add attributes (classes, ids, etc) to the existing HTML elements, or link stylesheets or additional scripts if necessary
- *
- * SEE BELOW FOR THE CATEGORIES OF THE TYPES OF FUNCTIONS YOU WILL BE CREATING 👇
- * 
- */
 
 /********** TEMPLATE GENERATION FUNCTIONS **********/
 
